409f64c5113a7ad3db780bc25ed7da5412192f03
alan3700 <alandaunay1@gmail.com>
2023-05-25 17:59:08 +0200

    fonction play

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
-import TOKEN from "./.env"
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits,VoiceChannel } = require('discord.js');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg-static');
 const dotenv = require('dotenv');
 
 const client = new Client({
@@ -12,14 +13,44 @@ const client = new Client({
 	],
 });
 
-const token = process.env.TOKEN;
+const token = ""
 
 client.on('ready', () => {
   console.log('Félicitations, Alfred démarre !');
 });
 
+client.on('messageCreate', async message => {
+  if (message.content.startsWith('!play')) {
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+      return message.reply('Please join a voice channel first.');
+    }
+
+    const videoUrl = message.content.slice(6).trim();
+    if (!ytdl.validateURL(videoUrl)) {
+      return message.reply('Invalid YouTube URL.');
+    }
+
+    const connection = await voiceChannel.join();
+    const stream = ytdl(videoUrl, { filter: 'audioonly' });
+    const dispatcher = connection.play(stream);
+
+    dispatcher.on('start', () => {
+      message.channel.send(`Now playing: ${videoUrl}`);
+    });
+
+    dispatcher.on('finish', () => {
+      voiceChannel.leave();
+    });
+
+    dispatcher.on('error', error => {
+      console.error(error);
+    });
+  }
+});
+  
+
 client.on('messageCreate', message => {
-  console.log(message)
   if (message.content === '!ping') {
     message.reply('Salut '+ message?.user?.username);
   }
